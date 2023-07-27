23f52bce6672dd01d3b2869464c6746dfc0ce876
hotterbeekxwama <119763307+hotterbeekxwama@users.noreply.github.com>
2023-07-28 01:59:20 +0200

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/home/Wiel600922319/wiel600922319.eu.pythonanywhere.com/static/css/stijlles5django.css"
+STATIC_URL = "/home/Wiel600922319/wiel600922319.eu.pythonanywhere.com/static/"
 
 
 
-STATIC_ROOT = BASE_DIR / "/home/Wiel600922319/wiel600922319.eu.pythonanywhere.com/mysite/staticfiles-cdn"
+STATIC_ROOT = BASE_DIR / "/home/Wiel600922319/wiel600922319.eu.pythonanywhere.com/mysite/staticfiles-cdn/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
