f76cd92a7b2822c9bebc010308c218ad1b7a1d7f
hotterbeekxwama <119763307+hotterbeekxwama@users.noreply.github.com>
2023-07-29 17:16:12 +0200

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/home/Wiel1960/wiel1960.pythonanywhere.com/mysite/blog/static/css/"
+STATIC_URL = "/home/Wiel1960/wiel1960.pythonanywhere.com/mysite/blog/static/"
 
 
 
